7a57d7eb11ace3cc2c0a3a9583dea276aacd0d7a
Bien Sarmiento <60124509+BnS2@users.noreply.github.com>
2026-04-15 18:14:13 +0800

    feat: migrated to Tanstack Start from Nextjs

---
+++ src/app/__root.tsx
@@ -0,0 +1,42 @@
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
+import appCss from "./globals.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: "utf-8" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStack Start Starter",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "stylesheet",
+        href: appCss,
+      },
+    ],
+  }),
+  component: RootLayout,
+});
+
+function RootLayout() {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
