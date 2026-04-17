7780a4bb87dc7a26d8eab9c551c368c1b41951dc
Bien Sarmiento <60124509+BnS2@users.noreply.github.com>
2026-04-17 20:26:55 +0800

    feat: Add protectedProcedure and session context to tRPC

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -1,11 +1,15 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { flattenError, ZodError } from "zod";
-// import { db } from "@/server/db";
+import { db } from "@/server/db";
+import { auth } from "../auth";
 
 export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const session = await auth.api.getSession({ headers: opts.headers });
+
   return {
-    // db,
+    db,
+    session,
     headers: opts.headers,
   };
 };
@@ -27,3 +31,14 @@ export const createCallerFactory = t.createCallerFactory;
 
 export const createTRPCRouter = t.router;
 export const publicProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+  if (!ctx.session?.user) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+  return next({
+    ctx: {
+      ...ctx,
+      user: ctx.session.user,
+    },
+  });
+});
